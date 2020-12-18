d268ec86c57c45438dd23258daa91a3303e4c028
Jihye Woo <woojihye2339@gmail.com>
2020-12-18 23:40:28 +0900

    [ beakjun2887 ] uncomplete version

---
+++ Hash/완주하지못한선수.py
@@ -0,0 +1,13 @@
+import collections
+
+
+def solution(participant, completion):
+    total = collections.Counter()
+    total.update(participant)
+    total.update(completion)
+
+    for key, val in total.items():
+        if val % 2 != 0:
+            return key
+
+    return ''
---
+++ MST/행성터널.py
@@ -0,0 +1,72 @@
+import heapq
+from sys import stdin
+
+
+def solution_BJ2887():
+    n = int(stdin.readline())
+    x, y, z = [], [], []
+    edge_heap = []
+    parent = [i for i in range(n)]
+    rank = [0 for i in range(n)]
+    total_value = 0
+    num_of_edge = 0
+
+    for i in range(n):
+        tmp = stdin.readline().split()
+        heapq.heappush(x, [int(tmp[0]), i])
+        heapq.heappush(y, [int(tmp[1]), i])
+        heapq.heappush(z, [int(tmp[2]), i])
+
+    make_edges(edge_heap, x)
+    make_edges(edge_heap, y)
+    make_edges(edge_heap, z)
+
+    while num_of_edge < n - 1:
+        weight = get_next_edge(edge_heap, parent, rank)
+        if weight != 0:
+            total_value += weight
+            num_of_edge += 1
+    print(total_value)
+
+    return total_value
+
+def make_edges(edge_heap, group):
+    prev = 0
+    for next in range(1, len(group)):
+        prev_val = group[prev][0]
+        next_val = group[next][0]
+        prev_idx, next_idx = group[prev][1], group[next][1]
+        heapq.heappush(edge_heap, (abs(prev_val - next_val), prev_idx, next_idx))
+        prev = next
+
+def get_next_edge(edge_heap, parent, rank):
+    edge = heapq.heappop(edge_heap)
+    weight, node1, node2 = edge
+    if node1 > node2:
+        node1, node2 = node2, node1
+
+    if find(parent, node1) != find(parent, node2):
+        union(rank, parent, node1, node2)
+        return True
+    else:
+        return False
+
+def find(parent, node):
+    if parent[node] != node:
+        parent[node] = find(parent, parent[node])
+    return parent[node]
+
+def union(rank, parent, node1, node2):
+    root1 = node1
+    root2 = node2
+
+    if rank[root1] > rank[root2]:
+        parent[root2] = root1
+    else:
+        parent[root1] = root2
+        if rank[root1] == rank[root2]:
+            rank[root2] += 1
+
+
+if __name__ == '__main__':
+    solution_BJ2887()
