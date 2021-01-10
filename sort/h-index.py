7d683c3bfc9fb096c753d049cc5abb61bb128ede
Jihye Woo <woojihye2339@gmail.com>
2021-01-10 19:40:55 +0900

    [programmers_h-index] - 성공   - 이럴 수가.. 쉬운 문제인데 너무 어렵게 생각했다.   👉 h-index : https://www.ibric.org/myboard/read.php?Board=news&id=270333

--- sort/h-index.py
+++ sort/h-index.py
@@ -1,17 +1,10 @@
 def solution(citations):
-    answer = 0
+    citations.sort()
+    length = len(citations)
+    for idx in range(length):
+        num_of_citation = citations[idx]
+        num_of_paper = length - idx
+        if num_of_citation >= num_of_paper:
+            return num_of_paper
 
-    # front, last = 0, max(citations)
-    front, last = 0, 10000
-
-    while front < last:
-        mid = front + last // 2
-        mid_result = sum([1 for c in citations if c >= mid])
-        if mid < mid_result:
-            front = mid + 1
-        else:
-            last = mid - 1
-
-    return front + 1
-
-print(solution(	[3, 0, 6, 1, 5]))
+    return 0
