5715bd4df31ea7603a61a2089fed7e7590ee163d
Jihye Woo <woojihye2339@gmail.com>
2021-01-13 23:35:38 +0900

    [leetcode_linked-list-cycle] - 성공

---
+++ code-squad/week02/linked-list-cycle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        visited = set()
+        current = head
+        while current:
+            if current in visited:
+                return True
+            visited.add(current)
+            current = current.next
+        return False
