902e29b65ca8eb97d53c239667ae438dfacaa1d4
Jihye Woo <woojihye2339@gmail.com>
2021-01-31 21:28:24 +0900

    [programmers_조이스틱] - 성공   - 백트래킹 경우의 수까지 추가해서 성공

--- greedy/조이스틱.py
+++ greedy/조이스틱.py
@@ -8,11 +8,29 @@ def solution(name):
 
     right_most_A = count_continuousA(reversed(name))
     left_most_A = count_continuousA(name[1:])
+    left = left_and_right - right_most_A
+    right = left_and_right - left_most_A
 
-    left = up_and_down + left_and_right - right_most_A
-    right = up_and_down + left_and_right - left_most_A
+    answer = min(left, right)
 
-    return min(left, right)
+    max_info = max_continuousA(name)
+    idx, max_len = max_info
+
+    if idx > 1: # front 쪽으로 가려고 해도 막혀있뜸
+        front = idx - 1
+        back = len(name) - (idx + max_len)
+
+        b_f_b = back * 2 + front
+        f_b_f = front * 2 + back
+
+        back_track_min = min(b_f_b, f_b_f)
+        answer = min(back_track_min, answer)
+
+    return answer + up_and_down
+    # left = up_and_down + left_and_right - right_most_A
+    # right = up_and_down + left_and_right - left_most_A
+    #
+    # return min(left, right)
 
 def count_continuousA(name, counter = 0):
     for c in name:
@@ -20,4 +38,12 @@ def count_continuousA(name, counter = 0):
         counter += 1
     return counter
 
-print(solution(	"BBBAAAAB"))
+def max_continuousA(name):
+    #(start_idx, max_len)
+    max_info = (-1, 0)
+    for idx in range(len(name)):
+        if name[idx] == 'A':
+            length = count_continuousA(name[idx:])
+            if max_info[1] < length:
+                max_info = (idx, length)
+    return max_info
