8def2cd9e2f04a8b0d191cc6cf4fd2e3807e9e6f
Jihye Woo <woojihye2339@gmail.com>
2021-02-14 18:35:07 +0900

    [programmers_등굣길] - 성공   - BFS 풀이   - 결과 값 mod 1_000_000_007

--- dp/등굣길.py
+++ dp/등굣길.py
@@ -1,24 +1,30 @@
+
+from collections import deque
 def solution(m, n, puddles):
     puddle_set = set()
-    global map
     map = [[0] * (m + 1) for _ in range(n + 1)]
-    map[1][1] = 0
+    map[1][1] = 1
+
     for x, y in puddles:
         puddle_set.add((y, x))
 
-    return dp(n, m, puddle_set)
+    queue = deque([(1, 1)])
 
-def dp(y, x, puddle_set):
+    while queue:
+        y, x = queue.popleft()
 
-    if (y, x) in puddle_set:
-        return 0
+        if (y, x) in puddle_set:
+            continue
+        else:
+            puddle_set.add((y, x))
 
-    if x == 1 and y == 1:
-        return 1
+        if y < n:
+            map[y + 1][x] += map[y][x]
+            queue.append((y + 1, x))
+        if x < m:
+            map[y][x + 1] += map[y][x]
+            queue.append((y, x + 1))
 
-    if x < 1 or y < 1:
-        return 0
-
-    return dp(y, x - 1, puddle_set) + dp(y - 1, x, puddle_set)
+    return map[n][m] % 1_000_000_007
 
 
