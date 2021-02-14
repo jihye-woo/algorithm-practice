d7abc764e5ab46f926f0406dc5b2e8fb5e0b7be2
Jihye Woo <woojihye2339@gmail.com>
2021-02-14 18:13:43 +0900

    [programmers_등굣길] - 실패   - recursion 풀이   - 효율성 테스트 실패

--- dp/등굣길.py
+++ dp/등굣길.py
@@ -1,28 +1,24 @@
-from collections import deque
 def solution(m, n, puddles):
     puddle_set = set()
+    global map
     map = [[0] * (m + 1) for _ in range(n + 1)]
-    map[1][1] = 1
-
+    map[1][1] = 0
     for x, y in puddles:
         puddle_set.add((y, x))
 
-    queue = deque([(1, 1)])
+    return dp(n, m, puddle_set)
 
-    while queue:
-        y, x = queue.popleft()
+def dp(y, x, puddle_set):
 
-        if (y, x) in puddle_set:
-            continue
-        else:
-            puddle_set.add((y, x))
+    if (y, x) in puddle_set:
+        return 0
 
-        if y < n:
-            map[y + 1][x] += map[y][x]
-            queue.append((y + 1, x))
-        if x < m:
-            map[y][x + 1] += map[y][x]
-            queue.append((y, x + 1))
+    if x == 1 and y == 1:
+        return 1
 
-    return map[n][m]
+    if x < 1 or y < 1:
+        return 0
+
+    return dp(y, x - 1, puddle_set) + dp(y - 1, x, puddle_set)
+
 
