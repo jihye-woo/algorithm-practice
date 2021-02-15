150d980e22efa7d5a11e7613632a917a41d65927
Jihye Woo <woojihye2339@gmail.com>
2021-02-16 02:03:31 +0900

    [programmers_도둑질] - 성공   - dp 사용   - dp[n] = max(dp[n - 1], dp[n - 2] + dp[n])

--- dp/도둑질.py
+++ dp/도둑질.py
@@ -1,14 +1,19 @@
-
 def solution(money):
-    memo = money.copy()
-    memo[0] = max(dp(1, memo), dp(2, memo) + memo[0])
 
-    return memo[0]
+    if len(money) == 3:
+        return max(money[0], money[1], money[2])
 
-def dp(n, memo):
-    if n >= len(memo):
-        return 0
+    # 첫 번째 집을 방문하는 경우
+    include_first = [money[0]]
+    include_first.append(max(money[0], money[1]))
 
-    return max(dp(n + 1, memo), dp(n + 2, memo) + memo[n])
+    for i in range(2, len(money) - 1):
+        include_first.append(max(include_first[i - 2] + money[i], include_first[i - 1]))
 
+    # 첫 번째 집을 방문하지 않는 경우
+    exclude_first = [0, money[1]]
 
+    for i in range(2, len(money)):
+        exclude_first.append(max(exclude_first[i - 2] + money[i], exclude_first[i - 1]))
+
+    return max(include_first[-1], exclude_first[-1])
